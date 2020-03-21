673949cceb604a19e018f28df008a996e5a37aa9
ColanNexcale <a@b.de>
2020-03-22 00:14:53 +0100

    added segeberg

---
+++ landkreise/get-segeberg.py
@@ -0,0 +1,17 @@
+import scrape
+import helper
+
+
+# (Labordiagnostisch bestätigt, Stand 21.03.2020)
+
+url = "https://www.segeberg.de/Quicknavigation/Startseite"
+prefix = "COVID-19-FÄLLE IM KREIS SEGEBERG:"
+prefix_date = "bestätigt, Stand"
+date_format = "(Labordiagnostisch bestätigt, Stand %d.%m.%Y)"
+case_func = lambda bs: helper.extract_case_num(bs.text, prefix)
+date_func = lambda bs: helper.extract_status_date(bs, prefix_date, date_format)
+
+scrape.scrape(url, "01060", case_func, date_func)
+
+
+
