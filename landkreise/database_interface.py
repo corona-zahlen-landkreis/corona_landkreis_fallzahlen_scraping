578538315eac57a52c9223bf7adc958ee65e5464
dadosch <dadosch@users.noreply.github.com>
2020-03-22 09:29:46 +0100

    fix databaseinterface

--- landkreise/database_interface.py
+++ landkreise/database_interface.py
@@ -1,6 +1,5 @@
 import os.path
 
-
 def add_to_database(uniqueId, status, cases, name="", parentId=None):
   if parentId == None:
     data_file = "data/"+uniqueId+".csv"
@@ -15,20 +14,30 @@ def add_to_database(uniqueId, status, cases, name="", parentId=None):
     f.close()
 
   with open(data_file, "r+") as file:
-    line_count = 0
+    status_is_in = False  
+    cases_is_in = False
+    existing_line = ""
+    
     for line in file:
-      line_count = line_count + 1
-      if status in line:
-        # verify/report value mismatch? (offer merge later, usually just overwrite with correct newer values?)
-        [line_status, line_cases] = line.split(",")
+      if(status in line):
+        status_is_in = True
+        if(","+str(cases)) in line:
+          cases_is_in = True
+          existing_line = line
+    
+    if status_is_in:
+      #print("nothing new")
+      
+      if not cases_is_in:
+      # there is already a line with the same status
+      # verify/report value mismatch? (offer merge later, usually just overwrite with correct newer values?)
+        [line_status, line_cases] = existing_line.split(",")
+        print("status: {}, cases:{}".format(line_status,line_cases))
         if int(line_cases) != cases:
-            print("ERROR in {}:{}: Collision with existing value {}={}, but new value is {}={}".format(data_file, line_count, line_status, line_cases, status, cases))
-        else:
-            # Value matches existing entry
-            break
+            print("ERROR in {}: Collision with existing value {}={}, but new value is {}={}".format(data_file, line_status, line_cases, status, cases))
+    else:
+      if parentId == None:
+        print("NEU: {}({}) hat {} Fälle, Stand {}".format(name, uniqueId, cases, status))
       else:
-        if parentId == None:
-          print("NEU: {}({}) hat {} Fälle, Stand {}".format(name, uniqueId, cases, status))
-        else:
-          print("NEU: {}({} teil von {}) hat {} Fälle, Stand {}".format(name, uniqueId, parentId, cases, status))
-        file.write(status+ ","+str(cases)+"\n")
+        print("NEU: {}({} teil von {}) hat {} Fälle, Stand {}".format(name, uniqueId, parentId, cases, status))
+      file.write(status+ ","+str(cases)+"\n")
