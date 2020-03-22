57acb1b94bab06de238561333f0927fa705afdb6
dadosch <dadosch@users.noreply.github.com>
2020-03-22 22:10:42 +0100

    fix augsburg

--- landkreise/get-augsburg-stadt.py
+++ landkreise/get-augsburg-stadt.py
@@ -11,14 +11,16 @@ main_url = "https://www.augsburg.de/umwelt-soziales/gesundheit/coronavirus"
 req = requests.get(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
-status_pattern = "Stand .* Uhr"
-cases_pattern = "[0-9]+ COVID-19-Fälle"
+#(Stand 22.3.2020, 09:30 Uhr)
+status_pattern = "Stand.*?Uhr"
+cases_pattern = "aktuell.*?[0-9]+.*?COVID"
 
-text = bs.findAll(text=re.compile(status_pattern))[0]
+text = bs.findAll('div',{'class':'csc-default disturb-container'})[1].getText()
+
 status_raw = re.findall(status_pattern,text)[0]
 status = datetime.datetime.strptime(status_raw, 'Stand %d.%m.%Y, %H:%M Uhr').strftime("%Y-%m-%d %H:%M:%S")
 
-cases_raw = bs.find(text=re.compile(cases_pattern))
+cases_raw = re.findall(cases_pattern, text)[0]
 cases = int(re.findall(r'[0-9]+', cases_raw)[0])
 
-add_to_database("09761", status, cases)
+add_to_database("09761", status, cases, "Stadt Augsburg")
