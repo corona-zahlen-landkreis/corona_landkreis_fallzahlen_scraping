cc7127d7f9220b8e5f2559f3d534cdf2fb840957
dadosch <dadosch@users.noreply.github.com>
2020-03-22 12:36:38 +0100

    leer kreis

--- landkreise/helper.py
+++ landkreise/helper.py
@@ -17,3 +17,14 @@ def extract_status_date_directregex(text, regexmatch, input_date_format, match, 
     print(text)
     status_raw = re.findall(regexmatch,text)[match]
     return datetime.datetime.strptime(status_raw, input_date_format).strftime(output_date_format)
+
+def get_table(table):
+    data = []
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
+
