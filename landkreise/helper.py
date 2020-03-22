d68e0d63e9fb7c4bc1368bb82acf68dc85bd9ab0
ColanNexcale <a@b.de>
2020-03-22 12:26:55 +0100

    date functions set locale to de_DE now, added function to handle missing years

--- landkreise/helper.py
+++ landkreise/helper.py
@@ -1,14 +1,27 @@
 import re
 import datetime
+import locale
+
+
+def check_and_replace_year(date_string):
+    if re.match("1900", date_string):
+        return date_string.replace("1900", datetime.date.today().strftime("%Y"))
+    else:
+        return date_string
 
 def extract_case_num(text, prefix):
     cases_raw = text.split(prefix)[1]
     return int(re.findall("[0-9]+", cases_raw)[0])
 
 def extract_status_date(bs, prefix, input_date_format):
+    locale.setlocale(locale.LC_ALL, "de_DE.utf-8")
     status_raw = bs.findAll(text=re.compile(prefix))[0]
-    return datetime.datetime.strptime(status_raw, input_date_format).strftime("%Y-%m-%d %H:%M:%S")
+    date_string = datetime.datetime.strptime(status_raw, input_date_format).strftime("%Y-%m-%d %H:%M:%S")
+    return check_and_replace_year(date_string)
 
 def extract_status_date_directregex(bs, regexmatch, input_date_format, match):
+    locale.setlocale(locale.LC_ALL, "de_DE.utf-8")
     status_raw = re.findall(regexmatch,bs.getText())[match]
-    return datetime.datetime.strptime(status_raw, input_date_format).strftime("%Y-%m-%d %H:%M:%S")
+    date_string = datetime.datetime.strptime(status_raw, input_date_format).strftime("%Y-%m-%d %H:%M:%S")
+    return check_and_replace_year(date_string)
+
