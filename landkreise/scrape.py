5785ba3e32c8158e53185400977d17f811d4faff
ColanNexcale <a@b.de>
2020-03-22 14:10:39 +0100

    added name and parent id to scrape function

--- landkreise/scrape.py
+++ landkreise/scrape.py
@@ -21,11 +21,11 @@ def extract_case_num(text, prefix):
     cases_raw = text.split(prefix)[1]
     return int(re.findall("[0-9]+", cases_raw)[0])   
 
-def scrape(url, community_id, cases_func, date_func = None):
+def scrape(url, community_id, cases_func, date_func = None, name="", parent_community_id=None):
     req = requests.get(url,headers=headers)
     bs = BeautifulSoup(req.text, "html.parser")
     date = time_stamp() if date_func is None else date_func(bs)
     cases = cases_func(bs)
-    add_to_database(community_id, date, cases)
+    add_to_database(community_id, date, cases, name, parent_community_id)
 
 
