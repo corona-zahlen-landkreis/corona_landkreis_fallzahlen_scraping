c2dc899aae1b23073184a72cef118607f496ceac
dadosch <dadosch@users.noreply.github.com>
2020-03-23 15:33:02 +0100

    fix zollernalbkreis

--- landkreise/get-zollernalbkreis.py
+++ landkreise/get-zollernalbkreis.py
@@ -11,15 +11,16 @@ main_url = "https://www.zollernalbkreis.de/aktuelles/nachrichten/antworten+auf+h
 req = requests.get(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
+text=bs.getText()
 
 #Gesamtzahl der an dem Coronavirus-Infizierten im Zollernalbkreis: 76 (
-cases_pattern = "Gesamtzahl der an dem Coronavirus-Infizierten im Zollernalbkreis: [0-9]+ \("
+cases_pattern = "Gesamtzahl der an dem Coronavirus-Infizierten im Zollernalbkreis: [0-9]+"
 
 status_raw = bs.findAll(text=re.compile("Stand:"))[0]
 status= datetime.datetime.strptime(status_raw, '(Stand: %d.%m.%Y, %H Uhr)').strftime("%Y-%m-%d %H:00:00")
 
 
-cases_raw=re.findall(cases_pattern, bs.find('div', {"id": "boxid16843898"}).getText() )[0]
+cases_raw=re.findall(cases_pattern, text)[0]
 cases = int(re.findall(r'[0-9]+', cases_raw)[0])
 
 add_to_database("08417", status, cases, "Zollernalbkreis")
