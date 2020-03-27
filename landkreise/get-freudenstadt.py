a235352173c073e4aece018a1912839fbb401902
dadosch <dadosch@users.noreply.github.com>
2020-03-28 00:09:13 +0100

    fix freudenstadt

--- landkreise/get-freudenstadt.py
+++ landkreise/get-freudenstadt.py
@@ -4,9 +4,9 @@ import requests
 import datetime
 import re
 
-from dateparser.search import search_dates
-import helper
 
+import helper
+import scrape
 import locale
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
 
@@ -14,15 +14,15 @@ from database_interface import *
 
 main_url = "https://www.landkreis-freudenstadt.de/385534.html"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
 cases_pattern = "auf [0-9]+ erhöht"
 
 text = helper.clear_text_of_ambigous_chars(bs.getText())
 
-status_raw = re.findall("\([0-9]+.*Uhr\)", text)[0]
-status= datetime.datetime.strptime(status_raw, '(%d. %B, %H Uhr)').strftime("2020-%m-%d")
+status_raw = re.findall("\([0-9]+.*\) eingegangenen", text)[0]
+status= helper.get_status(status_raw)
 
 
 
