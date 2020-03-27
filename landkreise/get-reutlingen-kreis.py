dd121dcd7e7f3ca7fbaf25202db521ccb00b2c1b
dadosch <dadosch@users.noreply.github.com>
2020-03-27 23:14:59 +0100

    fix reutlingen

--- landkreise/get-reutlingen-kreis.py
+++ landkreise/get-reutlingen-kreis.py
@@ -7,19 +7,21 @@ import re
 import locale
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
 
+import scrape
+import helper
 from database_interface import *
 
 main_url = "https://www.kreis-reutlingen.de/de/Aktuelles/Landkreis-aktuell/Landkreis-aktuell?view=publish&item=article&id=1923"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url, options={'cookies':{'skipEntranceUrl':'1'}})
 bs = BeautifulSoup(req.text, "html.parser")
 
 cases_pattern = "Gesamtzahl laborbestätigter Fälle: [0-9]+"
 
 text=bs.getText()
 
-status_raw = re.findall("Stand: .*2020", text)[0]
-status= datetime.datetime.strptime(status_raw, 'Stand: %d.%m.%Y').strftime("%Y-%m-%d")
+status_raw = re.findall("Stand: .*?2020", text)[0]
+status=helper.get_status(status_raw)
 
 cases_raw = re.findall(cases_pattern,text)[0]
 cases = int(re.findall(r'[0-9]+', cases_raw)[0])
