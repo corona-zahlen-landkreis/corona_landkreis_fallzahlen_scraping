71322351a6bfbec2c815c9d3a0d5cc8ec6db4973
Jonas Hess <jonas.hess@mailbox.org>
2020-03-30 05:50:07 +0200

    added landkreis ravensburg

---
+++ landkreise/get-ravensburg-kreis.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+import scrape
+import helper
+
+main_url = "https://www.rv.de/Politik+_+Verwaltung/aktuelles+zum+coronaviurs"
+
+prefix_case = "gibt es bei uns"
+community_id = "08436"
+
+case_func = lambda bs: helper.extract_case_num(bs.text, prefix_case)
+date_func = lambda bs: helper.extract_status_date_directregex(req.text, date_regex, date_format, 0)
+
+date_regex = r"\<meta name=\"date\" content=\"([^\"]+)\"\>"
+date_format = "%Y-%m-%d"
+
+req = scrape.request_url(main_url)
+bs = BeautifulSoup(req.text, "html.parser")
+
+scrape.scrape(main_url, community_id, case_func, date_func, "Kreis Ravensburg")
