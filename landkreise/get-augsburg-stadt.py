610f5d9162a9052ce32939ccb6949273e6410a44
dadosch <dadosch@users.noreply.github.com>
2020-03-31 01:11:11 +0200

    fix augsburg stadt

--- landkreise/get-augsburg-stadt.py
+++ landkreise/get-augsburg-stadt.py
@@ -4,21 +4,24 @@ import requests
 import datetime
 import re
 
+import scrape
+from helper import *
 from database_interface import *
 
 main_url = "https://www.augsburg.de/umwelt-soziales/gesundheit/coronavirus"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
 #(Stand 22.3.2020, 09:30 Uhr)
 status_pattern = "Stand.*?Uhr"
-cases_pattern = "aktuell.*?[0-9]+.*?COVID"
+cases_pattern = "bisher [0-9]+ COVID"
 
-text = bs.findAll('div',{'class':'csc-default disturb-container'})[1].getText().replace(" ","")
+text = remove_chars_from_text(bs.getText(),["\n"])
+text = clear_text_of_ambigous_chars(text)
 
 status_raw = re.findall(status_pattern,text)[0]
-status = datetime.datetime.strptime(status_raw, 'Stand%d.%m.%Y,%H:%MUhr').strftime("%Y-%m-%d %H:%M:%S")
+status = get_status(status_raw)
 
 cases_raw = re.findall(cases_pattern, text)[0]
 cases = int(re.findall(r'[0-9]+', cases_raw)[0])
