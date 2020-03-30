5a51b1f3d9707dd8a55c807db1c017698b63722d
dadosch <dadosch@users.noreply.github.com>
2020-03-31 01:34:05 +0200

    fix aachen

--- landkreise/get-aachen.py
+++ landkreise/get-aachen.py
@@ -7,11 +7,13 @@ import re
 import locale
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
 
+import scrape
+from helper import *
 from database_interface import *
 
 main_url = "http://www.aachen.de/DE/stadt_buerger/notfall_informationen/corona/aktuelles/index.html"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
 # note special char
@@ -21,8 +23,8 @@ cases_staedteregion_pattern = "Aktuell [0-9]+ bestätigte Coronafälle in der St
 text=bs.getText()
 
 
-status_raw = re.findall("zum Corona-Virus; .*? Uhr",text)[0]
-status= datetime.datetime.strptime(status_raw, 'zum Corona-Virus; %A, %d.%m., %H.%M Uhr').strftime("2020-%m-%d %H:%M")
+status_raw = re.findall("\w+, \d+\.\d+\.\d+, \d+ Uhr",text)[0]
+status= get_status(status_raw)
 
 cases_aachen_raw = re.findall(cases_aachen_pattern,text)[0]
 cases_aachen = int(re.findall(r'[0-9]+', cases_aachen_raw)[0])
@@ -30,5 +32,6 @@ cases_aachen = int(re.findall(r'[0-9]+', cases_aachen_raw)[0])
 cases_staedteregion_raw  = re.findall(cases_staedteregion_pattern,text)[0]
 cases_staedteregion = int(re.findall(r'[0-9]+', cases_staedteregion_raw)[0])
 
-add_to_database("05334002", status, cases_aachen, "Stadt Aachen")
-add_to_database("05334", status, cases_staedteregion, "Staedregion Aachen")
+
+add_to_database("05334002", status, cases_aachen, "Stadt Aachen", parentId="05334")
+add_to_database("05334", status, cases_staedteregion, "Staedteregion Aachen")
