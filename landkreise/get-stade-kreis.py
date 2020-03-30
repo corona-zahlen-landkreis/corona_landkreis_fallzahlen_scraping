5b1abf543c31e1cff6f2390ef0b469a4bf812335
dadosch <dadosch@users.noreply.github.com>
2020-03-31 00:41:57 +0200

    fix stade

--- landkreise/get-stade-kreis.py
+++ landkreise/get-stade-kreis.py
@@ -1,20 +1,30 @@
+from datetime import date, timedelta
+from bs4 import BeautifulSoup
+import re
+
 import scrape
 import helper
+from database_interface import *
 
 
 url = "https://www.landkreis-stade.de/portal/meldungen/coronavirus-aktuelle-lage-informationen-und-hinweise-des-gesundheitsamtes-landkreis-stade-901004416-20350.html?rubrik=901000006"
 
-regex = "[0-9]+ COVID-"
+regex = "[0-9]+ positiv getestete"
 
-date_regex="Stand .*?:[0-9]+"
-date_format = "Stand %d. %B %H:%M"
+req = scrape.request_url(url)
+bs = BeautifulSoup(req.text, "html.parser")
+text=bs.getText()
+
+
+# status is always yesterday 18:00
+status=(date.today() + timedelta(days=-1)).strftime('%Y-%m-%d 18:00:00')
+
 
 # todo healed
 
-case_func = lambda bs: helper.extract_case_num_directregex(bs.text, regex, 0)
-date_func = lambda bs: helper.extract_status_date_directregex(bs.text, date_regex, date_format, 0, "2020-%m-%d %H:%M:%S")
+cases = helper.get_number_only(re.findall(regex,text)[0])
 
-scrape.scrape(url, "03359", case_func, date_func, "Kreis Stade")
+add_to_database("03359", status, cases, "Kreis Stade")
 
 
 
