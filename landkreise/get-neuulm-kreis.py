8bb3ed863f46dc2bf997cc7e033f0a8b446650f4
dadosch <dadosch@users.noreply.github.com>
2020-03-31 01:21:53 +0200

    fix neuulm

--- landkreise/get-neuulm-kreis.py
+++ landkreise/get-neuulm-kreis.py
@@ -7,19 +7,21 @@ import re
 import locale
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
 
+import scrape
+from helper import *
 from database_interface import *
 
 main_url = "https://landkreis.neu-ulm.de/de/aktuelles-detail/insgesamt-18-faelle-fuer-eine-coronavirus-erkrankung-im-landkreis-neu-ulm-gemeldet.html"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
-cases_pattern = "Insgesamt [0-9]+ Fälle für"
+cases_pattern = "(?:Insgesamt [0-9]+ Fälle für)|(\d+ Personen im Landkreis)"
 
 text=bs.getText()
 
 status_raw = re.findall("Stand .*? Uhr",text)[0]
-status= datetime.datetime.strptime(status_raw, 'Stand %d.%m.%Y, %H:%M Uhr').strftime("%Y-%m-%d %H:%M")
+status= get_status(status_raw)
 
 cases_raw = re.findall(cases_pattern,text)[0]
 cases = int(re.findall(r'[0-9]+', cases_raw)[0])
