1962260f7519fe943316718b2e9eb4617fecfffb
dadosch <dadosch@users.noreply.github.com>
2020-03-31 01:50:24 +0200

    fix leer

--- landkreise/get-leer-kreis.py
+++ landkreise/get-leer-kreis.py
@@ -6,22 +6,23 @@ import re
 import locale
 
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
-import helper
 
+from helper import *
+import scrape
 from database_interface import *
 
 main_url = "https://www.landkreis-leer.de/Leben-Lernen/Coronavirus"
 
-req=requests.get(main_url)
+req=scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
 
 
 table = bs.find('table',{"data-bordercolor":"#949292"})
 
-data = helper.get_table(table)
+data = get_table(table)
 
-status_raw = re.findall("\(Stand .*? Uhr",bs.text)[0]
-status= datetime.datetime.strptime(status_raw, '(Stand %d. %B, %H.%M Uhr').strftime("2020-%m-%d %H:%M:%S")
+status_raw = re.findall("Stand .*? Uhr",bs.text)[0]
+status= get_status(status_raw)
 
 cases_total = int(data[13][1])
 
