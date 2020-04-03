7fb5424b8ec51cd666c1768c3a3c5aca3583bf8e
dadosch <dadosch@users.noreply.github.com>
2020-04-03 15:45:32 +0200

    fix muenchen stadt

--- landkreise/get-muenchen-stadt.py
+++ landkreise/get-muenchen-stadt.py
@@ -4,21 +4,24 @@ import requests
 import datetime
 import re
 
+import scrape
+from helper import *
 from database_interface import *
 
 main_url = "https://www.muenchen.de/aktuell/2020-03/coronavirus-muenchen-infektion-aktueller-stand.html"
 
-req = requests.get(main_url)
+req = scrape.request_url(main_url)
 bs = BeautifulSoup(req.text, "html.parser")
+raw_text=bs.getText()
 
 status_pattern = "(.*) Das Referat .*"
-cases_pattern = "[0-9|.]+ Infektionen"
+cases_pattern = "insgesamt.*?[0-9|.]+.*?Infektionen"
 
 text = bs.findAll(text=re.compile(status_pattern))[0]
-status_raw = re.findall(status_pattern,text)[0]
-status = datetime.datetime.strptime(status_raw, '(%d.%m.%Y)').strftime("%Y-%m-%d")
+status_raw = re.findall(status_pattern,text)[0].replace("(","").replace(")","")
+status = get_status(status_raw)
 
-cases_raw = bs.find(text=re.compile(cases_pattern))
-cases = int(re.findall(r'[0-9|.]+', cases_raw)[0].replace('.',''))
+cases_raw = bs.findAll(text=re.compile(cases_pattern))[0].replace('.','')
+cases = int(re.findall(r'[0-9|.]+', cases_raw)[0])
 
 add_to_database("09162000", status, cases, "Stadt München")
