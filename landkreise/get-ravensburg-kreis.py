87bce850affdab64112b8174632b2eedd23d061a
dadosch <dadosch@users.noreply.github.com>
2020-04-03 14:37:01 +0200

    fix ravensburg

--- landkreise/get-ravensburg-kreis.py
+++ landkreise/get-ravensburg-kreis.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import scrape
 import helper
 
-main_url = "https://www.rv.de/Politik+_+Verwaltung/aktuelles+zum+coronaviurs"
+main_url = "https://www.rv.de/Politik+_+Verwaltung/aktuelles+zum+coronavirus"
 
 prefix_case = "gibt es bei uns"
 community_id = "08436"
 
 case_func = lambda bs: helper.extract_case_num(bs.text, prefix_case)
-date_func = lambda bs: helper.extract_status_date_directregex(req.text, date_regex, date_format, 0)
+date_func = lambda bs: helper.extract_status_date_directregex(req.text, date_regex, date_format, 0, date_format)
 
 date_regex = r"\<meta name=\"date\" content=\"([^\"]+)\"\>"
 date_format = "%Y-%m-%d"
