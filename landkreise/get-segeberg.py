3c23210053a6bdc4d2318f4203e8750a9dd7127e
dadosch <dadosch@users.noreply.github.com>
2020-04-05 16:30:07 +0200

    fix segeberg

--- landkreise/get-segeberg.py
+++ landkreise/get-segeberg.py
@@ -1,17 +1,32 @@
-import scrape
-import helper
+from bs4 import BeautifulSoup
 
+import requests
+import datetime
+import re
+
+import scrape
+from helper import *
+from database_interface import *
 
 # (Labordiagnostisch bestätigt, Stand 21.03.2020)
 
 url = "https://www.segeberg.de/Quicknavigation/Startseite"
-prefix = "COVID-19-FÄLLE IM KREIS SEGEBERG:"
-prefix_date = "bestätigt, Stand"
-date_format = "(Labordiagnostisch bestätigt, Stand %d.%m.%Y)"
-case_func = lambda bs: helper.extract_case_num(bs.text, prefix)
-date_func = lambda bs: helper.extract_status_date(bs, prefix_date, date_format, "%Y-%m-%d")
 
-scrape.scrape(url, "01060", case_func, date_func, "Kreis Segeberg")
+req = scrape.request_url(url)
+bs = BeautifulSoup(req.text, "html.parser")
+
+prefix = "FÄLLE IM KREIS SEGEBERG: \d+"
+prefix_date = "Stand.*\)"
+
+text=bs.getText()
+
+status_raw = re.findall(prefix_date,text)[0].replace(")","")
+status = get_status(status_raw)
+
+cases_raw = re.findall(prefix,text)[0]
+cases = get_number_only(cases_raw)
+
+add_to_database("01060", status, cases, name="Kreis Segeberg")
 
 
 
